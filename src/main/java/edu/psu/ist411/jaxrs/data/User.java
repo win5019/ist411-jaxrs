cc28bae8e196780b847af35b35ecc65ecd0d7de0
Tyler <tylersuehr@gmail.com>
2018-07-01 14:11:58 -0400

    Created data layer for project

---
+++ src/main/java/edu/psu/ist411/jaxrs/data/User.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.psu.ist411.jaxrs.data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Represents a standard user.
+ * 
+ * @author Tyler Suehr
+ * @author David Wong
+ * @author Steven Weber
+ * @author Win Ton
+ */
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private long id;
+    @Column(name = "user_email")
+    private String email;
+    @Column(name = "user_first")
+    private String firstName;
+    @Column(name = "user_last")
+    private String lastName;
+    
+    
+    public User() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/main/java/edu/psu/ist411/jaxrs/data/UserRepository.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.psu.ist411.jaxrs.data;
+
+import java.awt.print.Pageable;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository for entity model {@link User}.
+ * 
+ * @author Tyler Suehr
+ * @author David Wong
+ * @author Steven Weber
+ * @author Win Ton
+ */
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    Page<User> findAll(Pageable pageable);
+    Optional<User> findByEmail(String email);
+}
