f7c596d49e2a835124702825b693111ab7aef20b
Tyler <tylersuehr@gmail.com>
2018-07-01 14:14:47 -0400

    Init spring app

--- src/main/java/edu/psu/ist411/jaxrs/Main.java
+++ src/main/java/edu/psu/ist411/jaxrs/Main.java
@@ -6,11 +6,20 @@
 
 package edu.psu.ist411.jaxrs;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
- *
+ * Starting point of Spring Boot application.
+ * 
  * @author Tyler Suehr
  * @author David Wong
+ * @author Steven Weber
+ * @author Win Ton
  */
+@SpringBootApplication
 public class Main {
-    
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class);
+    }
 }
